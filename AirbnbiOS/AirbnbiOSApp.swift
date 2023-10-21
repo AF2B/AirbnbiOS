120198bb91f598025d6f041ecbf5ff86a557f991
andre.borba <dev_andre.filipe@outlook.com>
2023-10-21 17:47:30 -0300

    Initial Commit

---
+++ AirbnbiOS/AirbnbiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbiOSApp.swift
+//  AirbnbiOS
+//
+//  Created by André Filipe Fonsêca Borba on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
