7c5ce8f07888e484fdcbc9c5a9a79c7f078a77f1
andre.borba <dev_andre.filipe@outlook.com>
2023-10-22 00:34:04 -0300

    feat: profile option row view

---
+++ AirbnbiOS/Core/ProfileOptionRowView.swift
@@ -0,0 +1,34 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbiOS
+//
+//  Created by André Filipe Fonsêca Borba on 22/10/23.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Title")
+}
